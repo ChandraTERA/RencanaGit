5bd1a29544076b4f4dc66bff9566a3235e70242d
Chandra TERA <Chandrawpsiloto@gmail.com>
2024-06-02 16:15:44 +0700

    Menambahkan index login

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:  [
+  "./css/**/*.{html,js}",
+  "./layouts/**/*.blade.php",
+  "./component-js/**/*.vue",
+],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
